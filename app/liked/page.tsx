b5157a56d1b3171dcf7828222b5e359decdc5bf6
ks27082002 <krishna.sharma21b@iiitg.ac.in>
2024-03-28 23:04:16 +0530

    final

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -8,7 +8,7 @@ const Liked = async () => {
   const songs = await getLikedSongs()
   return (
   <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
-    <Header>
+    <Header className="">
       <div className="mt-20">
         <div className="flex flex-col md:flex-row items-center gap-x-5">
           <div className="relative h-32 w-32 lg:h-44 lg:w-44">
