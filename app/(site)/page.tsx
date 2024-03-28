5f4f6bc975cdc1ab702d3e4f39315fe669b23cad
ks27082002 <krishna.sharma21b@iiitg.ac.in>
2024-03-28 22:59:56 +0530

    fix

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   const songs = await getSongs()
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto" >
-      <Header>
+      <Header className="">
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">
             Welcome Back
